d863694b2c1a9a557d86cd4d6b09f59559e450d8
Chandler <lauc21@up.edu>
2020-10-04 18:41:18 -0700

    Game State class

---
+++ app/src/main/java/edu/up/gamestate/GameState.java
@@ -0,0 +1,16 @@
+package edu.up.gamestate;
+
+public class GameState {
+    public void makeGame(){
+        Card[] player1Hand = new Card[8];
+        Card[] player2Hand = new Card[8];
+        Card[] player3Hand = new Card[8];
+        Card[] player4Hand = new Card[8];
+
+        Card[] discardPile = new Card[1];
+        Card[] deck = new Card[100];
+
+        int whoseTurn = 1;
+
+    }
+}
