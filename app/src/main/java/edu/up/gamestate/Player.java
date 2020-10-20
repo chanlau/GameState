40a84f3cb4bcc1758eefe3857fc5c0d45cc44d88
Chandler <lauc21@up.edu>
2020-10-19 19:18:01 -0700

    added check for exploding kitten in player and change turn methods

--- app/src/main/java/edu/up/gamestate/Player.java
+++ app/src/main/java/edu/up/gamestate/Player.java
@@ -9,4 +9,12 @@ public class Player {
         this.playerNum = num;
     }
 
+    public boolean checkForExplodingKitten(){
+        for(int i = 0; i <= this.playerHand.length; i++){
+            if(playerHand[i].getCardType() == 0){
+                return true;
+            }
+        }
+        return false;
+    }
 }
